cbc1afbd4cdf17fe020d9e5027ffffd43c475433
saniazt <saniazt@gmail.com>
2023-08-21 17:29:48 +0300

    mysql properties connected

---
+++ src/main/java/com/saniazt/rest/webservices/restfulwebservices/todo/repositories/TodoRepository.java
@@ -0,0 +1,13 @@
+package com.saniazt.rest.webservices.restfulwebservices.todo.repositories;
+import com.saniazt.rest.webservices.restfulwebservices.todo.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo,Integer> {
+
+    List<Todo> findByUsername(String username);
+
+}
